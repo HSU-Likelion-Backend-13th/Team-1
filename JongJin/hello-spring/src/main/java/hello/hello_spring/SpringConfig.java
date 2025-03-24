23e79cb50ef4ee065331cd6c28108a2aeb401bf7
JongJin <2171266@hansung.ac.kr>
2025-03-24 16:00:09 +0900

    [섹션 7] 5. JPA

--- JongJin/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ JongJin/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,23 +1,23 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMeberRepository;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    // @PersistenceContext : 생략해도 해줌
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -29,6 +29,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         // return new MemoryMeberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberRepository(dataSource);
+        // return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
