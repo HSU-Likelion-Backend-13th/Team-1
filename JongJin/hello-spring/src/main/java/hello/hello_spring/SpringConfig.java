ae9adf0bed31e1d81c62fdc33b1209896f5370f0
JongJin <2171266@hansung.ac.kr>
2025-03-24 16:44:59 +0900

    [섹션 7] 6. 스프링 데이터 JPA

--- JongJin/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ JongJin/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,10 +1,7 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,24 +9,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    // @PersistenceContext : 생략해도 해줌
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMeberRepository();
-        // return new JdbcMemberRepository(dataSource);
-        // return new JdbcMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        // return new MemoryMeberRepository();
+//        // return new JdbcMemberRepository(dataSource);
+//        // return new JdbcMemberRepository(dataSource);
+//        // return new JpaMemberRepository(em);
+//
+//    }
 }
