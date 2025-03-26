d9c3a8f977780a916740b6cb34737668854c5de0
EunJi <univ217@naver.com>
2025-03-26 20:31:12 +0900

    [섹션3] 3. API

--- Eunji/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ Eunji/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,31 @@ public class HelloController {
     public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model) { //외부에서 파라미터 받는 경우 , required = false일 시 값을 넘기지 않아도됨
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string") //이걸 쓸일 거의 없음
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
