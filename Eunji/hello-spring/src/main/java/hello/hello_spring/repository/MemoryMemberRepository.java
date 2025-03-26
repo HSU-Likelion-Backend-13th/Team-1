61272a58592cf2854b92b89f1bb0a86aaad98085
EunJi <univ217@naver.com>
2025-03-27 00:11:30 +0900

    [섹션7] 3-4. 스프링 통합테스트, Jdbc Template

--- Eunji/hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ Eunji/hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -18,21 +18,21 @@ public class MemoryMemberRepository implements MemberRepository {
         store.put(member.getId(), member);
         return member;
     }
-
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(store.get(id)); //반환값이 Null일수있는 경우에는 Optional 사용
+        return Optional.ofNullable(store.get(id));
     }
-
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
-
-    @Override
-    public List<Member> findAll() {
-        return new ArrayList<>(store.values());
+    public void clearStore() {
+        store.clear();
     }
 }
--- Eunji/hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
+++ Eunji/hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -1,31 +1,20 @@
 package hello.hello_spring.service;
 
-
 import hello.hello_spring.domain.Member;
-import hello.hello_spring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import hello.hello_spring.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
-
-class MemberServiceTest {
-
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
-
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
     @Test
     public void 회원가입() throws Exception {
         //Given
@@ -39,17 +28,14 @@ class MemberServiceTest {
     }
     @Test
     public void 중복_회원_예외() throws Exception {
-        //given
+        //Given
         Member member1 = new Member();
         member1.setName("spring");
-
         Member member2 = new Member();
         member2.setName("spring");
-        //when
+        //When
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-
-    }
-}
+    } }
