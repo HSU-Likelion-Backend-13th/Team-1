a8a2a2fd30d29811b983d69ee198fb76489f03e0
EunJi <univ217@naver.com>
2025-03-26 19:42:45 +0900

    [섹션3] 2. MVC와 템플릿 엔진

--- Eunji/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ Eunji/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!"); //모델은 MVC의 M
         return "hello"; //hello.html찾아서 걔한테가서 렌더링
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model) { //외부에서 파라미터 받는 경우 , required = false일 시 값을 넘기지 않아도됨
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
