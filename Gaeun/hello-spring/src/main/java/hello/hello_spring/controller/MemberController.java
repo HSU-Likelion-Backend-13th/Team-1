e1ed175176629464b9c01128dd2ddd6373bfaae5
LgE02 <leegaeun023@gmail.com>
2025-03-26 11:52:57 +0900

    [섹션 6] 회원 관리 예제 - 웹 MVC 개발 작성

--- Gaeun/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ Gaeun/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 //해당 클래스가 컨트롤러임을 나타냄
@@ -18,5 +24,29 @@ public class MemberController {
     //클래스가 스프링 빈으로 등록이 안되어있다면 작동하지 않는다.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    //조회시 Get 사용
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm.html";
+    }
+
+    //데이터 등록시 Post 사용
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/"; //홈 화면으로 이동
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberlist";
+
     }
 }
