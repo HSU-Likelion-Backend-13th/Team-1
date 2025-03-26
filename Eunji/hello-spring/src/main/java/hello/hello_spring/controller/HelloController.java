6fca21cbaefd69d5eb7d1784f2c32cb4303fed91
EunJi <univ217@naver.com>
2025-03-26 18:10:49 +0900

    [섹션2] 3. View 환경설정

---
+++ Eunji/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello") //웹앱에서 /hello라고 들어오면 이 메서드를 호출해줌(스프링이) Get은 Get,Post할때의 Get이다.
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!"); //모델은 MVC의 M
+        return "hello"; //hello.html찾아서 걔한테가서 렌더링
+    }
+}
