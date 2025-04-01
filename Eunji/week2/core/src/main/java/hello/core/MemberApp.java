0b42801b6b70f4f8a19b696e6d143e26463d717e
EunJi <univ217@naver.com>
2025-04-01 23:55:01 +0900

    2-(6) 스프링으로 전환

--- Eunji/week2/core/src/main/java/hello/core/MemberApp.java
+++ Eunji/week2/core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,16 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp { //테스트
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig(); -> 개발자가 직접 Appconfig를 사용해서 객체를 생성하고 의존성 주입
+//        MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "lion", Grade.VIP);
         memberService.join(member);
